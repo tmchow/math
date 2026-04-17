90c077cef0e32d33a73939beef55618753eb563d
Stan BuildBot <mc.stanislaw@gmail.com>
2026-04-17 07:34:32 -0400

    auto generated docs from Jenkins

---
+++ navtreedata.js
@@ -0,0 +1,96 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Stan Math Library", "index.html", [
+    [ "Overview", "index.html", null ],
+    [ "Contributor Guides", "md__8github_2_c_o_n_t_r_i_b_u_t_i_n_g.html", [
+      [ "Developer Guide", "md_doxygen_2contributor__help__pages_2developer__doc.html", null ],
+      [ "Adding New Functions", "md_doxygen_2contributor__help__pages_2getting__started.html", null ],
+      [ "Adding New Distributions", "md_doxygen_2contributor__help__pages_2adding__new__distributions.html", null ],
+      [ "Common Pitfalls", "md_doxygen_2contributor__help__pages_2common__pitfalls.html", null ],
+      [ "Using requires for general overloads", "md_doxygen_2contributor__help__pages_2require__meta.html", null ],
+      [ "Reverse Mode Types", "md_doxygen_2contributor__help__pages_2reverse__mode__types.html", null ],
+      [ "Testing Automatic Differentiation Functions", "md_doxygen_2contributor__help__pages_2autodiff__test__guide.html", null ],
+      [ "Testing New Distributions", "md_doxygen_2contributor__help__pages_2distribution__tests.html", null ],
+      [ "Add New Functions With Known Gradients", "md_doxygen_2contributor__help__pages_2new__gradients.html", null ],
+      [ "Adding New OpenCL Functions", "md_doxygen_2contributor__help__pages_2add__new__opencl__kernel.html", null ],
+      [ "Windows Development Tips", "md_doxygen_2contributor__help__pages_2windows__devnotes.html", null ]
+    ] ],
+    [ "Internal Docs", "modules.html", "modules" ],
+    [ "Parallelism", "usergroup0.html", [
+      [ "Threading", "md_doxygen_2parallelism__support_2threading__tbb.html", null ],
+      [ "OpenCL for GPU Computing", "md_doxygen_2parallelism__support_2opencl__support.html", null ],
+      [ "MPI Support", "md_doxygen_2parallelism__support_2mpi__parallelism.html", null ]
+    ] ],
+    [ "(External Link) Stan Language Docs", "^https://mc-stan.org/users/documentation/", null ],
+    [ "(External Link) Stan Discourse", "^https://discourse.mc-stan.org/", null ],
+    [ "Stan Math Library Docs", "index.html", "index" ],
+    [ "Class List", "annotated.html", "annotated" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classstan_1_1math_1_1addition__.html#a482f118c2715add20a217739af0303c6",
+"classstan_1_1math_1_1as__column__vector__or__scalar___ae2eca98eac9692c15ecde6b182b4b7ab.html#ae2eca98eac9692c15ecde6b182b4b7ab",
+"classstan_1_1math_1_1block__.html#a88df743411df9dae24cab368e2de0809",
+"classstan_1_1math_1_1check__cl___ad1c6eab6958d083a76fb5ee2f691ff55.html#ad1c6eab6958d083a76fb5ee2f691ff55",
+"classstan_1_1math_1_1constant__.html#ad6bcda01250a34fc96c75099dd9a86ef",
+"classstan_1_1math_1_1diagonal__.html#a002b8ac9791c4cdfd89f9f6aa1be03ad",
+"classstan_1_1math_1_1erf__.html#a827a88bf5c4d055e76b8b94985683757",
+"classstan_1_1math_1_1floor__.html#ac9b76c10a8638782df6e3e3611bc336c",
+"classstan_1_1math_1_1hypot___a526f518f87eaf784a3ed22f25a4c3357.html#a526f518f87eaf784a3ed22f25a4c3357",
+"classstan_1_1math_1_1internal_1_1log__inv__logit__diff__dv__vari_ab491407b5821ae918b52eb9a5d0e2ae5.html#ab491407b5821ae918b52eb9a5d0e2ae5",
+"classstan_1_1math_1_1internal_1_1ops__partials__edge_3_01double_00_01_op_00_01require__eigen__st_3_01is__var_00_01_op_01_4_01_4.html",
+"classstan_1_1math_1_1isfinite__.html#a14e9c88f9fbba06a8d1678022f2ffd1b",
+"classstan_1_1math_1_1lgamma__.html#a14e9c88f9fbba06a8d1678022f2ffd1b",
+"classstan_1_1math_1_1log1p__.html#a718d16386c97a7fae800d72a53c1a22a",
+"classstan_1_1math_1_1logical__and___a2da27412240f306c602ccc0cab0bfc3c.html#a2da27412240f306c602ccc0cab0bfc3c",
+"classstan_1_1math_1_1mpi__parallel__call_a844f693133414dba40757b121941a964.html#a844f693133414dba40757b121941a964",
+"classstan_1_1math_1_1operands__and__partials_3_01_op1_00_01_op2_00_01_op3_00_01_op4_00_01_op5_00205e3dfc4aa428638a4e542fee8c2c7d.html",
+"classstan_1_1math_1_1prod__2d__.html#aecdc105d2898995daf7445b5d12384b5",
+"classstan_1_1math_1_1rowwise__prod__.html#a9249ec7497c4015626cde05c5324c8a6",
+"classstan_1_1math_1_1sin__.html#abde9005c2e63baf1a70f25f5e5e72a84",
+"classstan_1_1math_1_1subtraction__operator__.html#a75dffa28b78be287550f7f16649a4028",
+"classstan_1_1math_1_1trunc__.html#abde9005c2e63baf1a70f25f5e5e72a84",
+"classstan_1_1math_1_1vari__value_3_01_t_00_01require__all__t_3_01is__plain__type_3_01_t_01_4_00_668033d9a4bfddcaba56f859af1eb1d1.html#a27811382fb8f437293b8161b1eae283a",
+"classstan_1_1math_1_1vari__view__eigen_a747a6bb5ce0108d4b558c76fdc1058b5.html#a747a6bb5ce0108d4b558c76fdc1058b5",
+"group__constructible__types_gabde2d2c17abcc503b316f4e6df4b3239.html#gabde2d2c17abcc503b316f4e6df4b3239",
+"group__opencl__kernel__generator_ga0ba76fcd6f07dba878951e8e2f078076.html#ga0ba76fcd6f07dba878951e8e2f078076",
+"group__opencl__kernels_ga670e8f7e81af969323f1cfd098d5b1d6.html#ga670e8f7e81af969323f1cfd098d5b1d6",
+"group__prob__dists_ga3dd2a9fb1a0a2118a50d516888986f0b.html#ga3dd2a9fb1a0a2118a50d516888986f0b",
+"group__type__trait_ga3e24dfe00dd09775b290fc2221b2739d.html#ga3e24dfe00dd09775b290fc2221b2739d",
+"struct_eigen_1_1internal_1_1general__matrix__vector__product_3_01_index_00_01stan_1_1math_1_1vard0ee77c2495b267f4203bf064098df76_afda401dfe88b5fe840a7a54eecda021a.html#afda401dfe88b5fe840a7a54eecda021a",
+"structstan_1_1math_1_1_autodiff_stack_singleton_a8fcca56f0d4ab8cae964513e1d523c84.html#a8fcca56f0d4ab8cae964513e1d523c84",
+"structstan_1_1math_1_1idas__service_a395e4cdb6826bfb4acc6bf0ef59eee68.html#a395e4cdb6826bfb4acc6bf0ef59eee68",
+"structstan_1_1math_1_1internal_1_1multi__result__kernel__internal_1_1inner.html",
+"structstan_1_1math_1_1promote__elements_3_01_eigen_1_1_matrix_3_01_t_00_01_r_00_01_c_01_4_00_01_4bbb5eeca2df3769281e2a5aeb9e13c3.html",
+"structstd_1_1numeric__limits_3_01stan_1_1math_1_1var__value_3_01_t_01_4_01_4_af953de0011256c4b7628fe0ec87bdfb5.html#af953de0011256c4b7628fe0ec87bdfb5"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex35.js
@@ -0,0 +1,7 @@
+var NAVTREEINDEX35 =
+{
+"structstd_1_1numeric__limits_3_01stan_1_1math_1_1var__value_3_01_t_01_4_01_4_af953de0011256c4b7628fe0ec87bdfb5.html#af953de0011256c4b7628fe0ec87bdfb5":[7,2,5,14],
+"structstd_1_1numeric__limits_3_01stan_1_1math_1_1var__value_3_01_t_01_4_01_4_af9b90de7a9a74628d51aafd714fe6e93.html#af9b90de7a9a74628d51aafd714fe6e93":[7,2,5,1],
+"structstd_1_1numeric__limits_3_01stan_1_1math_1_1var__value_3_01_t_01_4_01_4_afaa0c54c70b6fdfc0c007b8961643f55.html#afaa0c54c70b6fdfc0c007b8961643f55":[7,2,5,31],
+"usergroup0.html":[3]
+};
